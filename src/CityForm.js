d99c7864af582624d6f6652101693d6e94ba654f
Derek Douglas <derek.j.douglas13@gmail.com>
2022-05-11 23:30:23 -0500

    additional styling done

--- src/CityForm.js
+++ src/CityForm.js
@@ -8,14 +8,21 @@ class CityForm extends React.Component {
   render() {
     return (
       <div>
-        <Form className='center'style={{ width: '50rem' }}>
-          <Form.Control onChange={this.props.changeHandler}
-          placeholder= 'What City?'
-          />
-          <Button onClick={this.props.locationQuery}>Explore!</Button>
-        </Form>
+        <div className='userInput'>
+          <Form className='inputForm'style={{ width: '50rem' }}>
+            <Form.Control onChange={this.props.changeHandler}
+              placeholder= 'What City?'
+            />
+            <div className="d-grid gap-2">
+              <Button className='formButton' onClick={this.props.locationQuery}>
+                Explore!
+              </Button>
+            </div>
+          </Form>
+        </div>
         {this.props.locationName &&
         <Card className='center' style={{ width: '40rem' }}>
+          <Card.Img src={this.props.mapValue}/>
           <Card.Title>You searched for: {this.props.locationName.display_name}</Card.Title>
           <Card.Text>Lattitude: {this.props.locationName.lat} </Card.Text>
           <Card.Text>Longitude: {this.props.locationName.lon} </Card.Text>
