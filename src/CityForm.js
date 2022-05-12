b244c7f416fbd848f132007dbb54b15d2d5ae9d7
Derek Douglas <derek.j.douglas13@gmail.com>
2022-05-11 19:27:43 -0500

    adds location data functionality in its earliest form

--- src/CityForm.js
+++ src/CityForm.js
@@ -1,11 +1,30 @@
+import axios from 'axios';
 import React from 'react';
 
 class CityForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: '',
+      locationName: ''
+    }
+  }
+
+  locationQuery = async () => {
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+    const response = await axios.get(url);
+    console.log('Response from Axios: ', response.data[0].display_name);
+    this.setState({ locationName: response.data[0].display_name});
+  }
 
   render() {
     return (
       <>
-        <h1>City Explorer</h1>
+        <input onChange={(event) => this.setState({ searchQuery: event.target.value })} placeholder= 'What City?' />
+        <button onClick={this.locationQuery}>Take Me There!</button>
+        {this.state.locationName &&
+          <h2>You searched for {this.state.locationName}</h2>
+        }
       </>
     );
   }
